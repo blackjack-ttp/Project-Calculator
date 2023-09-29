e0be2d765989aa22852ee9911ee28896dfd6e0ca
blackjack-ttp <phongtt120298@outlook.com>
2023-09-29 22:48:18 +0700

    #2 [Feature] Screen Component and edit file SCSS Wrapper Component

---
+++ src/components/screen/index.jsx
@@ -0,0 +1,16 @@
+import { useContext } from 'react';
+import { CalcContext } from '../context/CalcContext';
+import { Textfit } from 'react-textfit';
+import './style.scss';
+
+const Screen = () => {
+  const { calc } = useContext(CalcContext);
+
+  return (
+    <Textfit className="screen" max={70} mode="single">
+      {calc.num ? calc.num : calc.res}
+    </Textfit>
+  );
+};
+
+export default Screen;
