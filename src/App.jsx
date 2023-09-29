92e8c69fec0c51663a54d160351957faffbc453a
blackjack-ttp <phongtt120298@outlook.com>
2023-09-29 22:42:14 +0700

    #2 [Feature] Wrapper Component

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,16 @@
 import './App.scss';
+import Wrapper from '@/components/wrapper';
 
 function App() {
-  return <>
-  <div className="App">
-    <h1>Hello, welcome to the project calculator</h1>
-  </div>
-  </>;
+  return (
+    <>
+      <div className="App">
+        <Wrapper>
+          <h1>Hello, welcome to the project calculator</h1>
+        </Wrapper>
+      </div>
+    </>
+  );
 }
 
 export default App;
---
+++ src/components/wrapper/index.jsx
@@ -0,0 +1,13 @@
+/* eslint-disable no-unused-vars */
+import React from 'react';
+import './style.scss';
+
+const Wrapper = ({ children }) => {
+  return (
+    <>
+      <div className="wrapper">{children}</div>
+    </>
+  );
+};
+
+export default Wrapper;
