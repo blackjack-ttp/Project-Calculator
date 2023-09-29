bc3f5813a712af7f006553723739932af455963b
blackjack-ttp <phongtt120298@outlook.com>
2023-09-29 23:05:15 +0700

    #2 [Feature] ButtonBox Component and edit file SCSS Wrapper Component

---
+++ src/components/buttonBox/index.jsx
@@ -0,0 +1,12 @@
+/* eslint-disable no-unused-vars */
+import React from 'react';
+
+const ButtonBox = ({ children }) => {
+  return (
+    <>
+      <div className="buttonBox">{children}</div>
+    </>
+  );
+};
+
+export default ButtonBox;
